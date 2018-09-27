0026d67edd8cc94a36dd944297b1b8baf1fb4c8b
nextorr <nextorr@gmail.com>
2018-09-26 21:21:46 -0500

    numero elementos 2 rojo
    paso 2 - se crea test para un elemento (ROJO)

--- ciclo1.py
+++ ciclo1.py
@@ -6,3 +6,5 @@ class ciclo1(TestCase):
     def test_numeroElementos(self):
         self.assertEqual(Arreglo().numeroElementos(""),0,"calculo de elementos vacio")
 
+    def test_un_numero(self):
+        self.assertEqual(Arreglo().numeroElementos("1"), 1, "Calculo de Un elemento")
