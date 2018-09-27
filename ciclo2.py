e59c2d905699e861f9ce001bfe74dc804f2b824b
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2018-09-26 22:02:46 -0500

    Paso 2:  Se crea prueba que retorne arreglo dos elementos cantidad y número minimo (ROJO)

--- ciclo2.py
+++ ciclo2.py
@@ -5,3 +5,6 @@ from Arreglo import Arreglo
 class ciclo2(TestCase):
     def test_numeroElementos(self):
         self.assertEqual(Arreglo().numeroElementos(""),[0,0],"calculo de elementos vacio")
+
+    def test_unNumeroUnMinimo(self):
+        self.assertEqual(Arreglo().numeroElementos("1"), [1, 1], "calculo de elementos 1 y numero minimo 1")
