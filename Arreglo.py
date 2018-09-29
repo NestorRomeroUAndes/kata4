d4ffef270ddb6f8a24a751a754d2733e806cf4a1
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2018-09-28 21:08:42 -0500

    Paso 1:  Se retorna arreglo de tres elementos (VERDE)

--- Arreglo.py
+++ Arreglo.py
@@ -5,7 +5,7 @@ class Arreglo:
         cadena2 = cadena.split(",")
         tamano = len(cadena2)
         if len(cadena) == 0:
-            return [0,0]
+            return [0,0,0]
         else:
             minimo = int(min(cadena2))
             return [tamano,minimo]
