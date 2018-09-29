ba849c72631e59051dc1d96b4ec2762d4db32f75
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2018-09-28 21:48:12 -0500

    Paso 2:  Se crea caso de prueba retorna arreglo con cuatro valores en uno (ROJO)

--- ciclo4.py
+++ ciclo4.py
@@ -5,3 +5,7 @@ from Arreglo import Arreglo
 class ciclo4(TestCase):
     def test_cero_elementos(self):
         self.assertEqual(Arreglo().numeroElementos(""),[0,0,0,0],"calculo de elementos vacio y todo lo demas cero")
+
+    def test_unNumeroUnMinimoUnMaximoPromedio(self):
+        self.assertEqual(Arreglo().numeroElementos("1"), [1, 1, 1, 1],
+                         "calculo de elementos 1 numero minimo 1 numero maximo 1 promedio 1")
