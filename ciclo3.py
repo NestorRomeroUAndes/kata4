1f10de6c6f7d3594d68e19cf685a418b5b4bd196
nextorr <nextorr@gmail.com>
2018-09-28 21:31:15 -0500

    paso3 se crea prieba para 2 elementos retornando el numero, el minimo y el maximo (ROJO)
    paso3 se crea prieba para 2 elementos retornando el numero, el minimo y el maximo (ROJO)

--- ciclo3.py
+++ ciclo3.py
@@ -8,3 +8,6 @@ class ciclo2(TestCase):
 
     def test_unNumeroUnMinimoUnMaximo(self):
         self.assertEqual(Arreglo().numeroElementos("1"), [1, 1, 1], "calculo de elementos 1 numero minimo 1 numero maximo 1")
+
+    def test_DosNumeroUnMinimoUnMaximo(self):
+        self.assertEqual(Arreglo().numeroElementos("1,3"), [2, 1, 3], "calculo de dos elementos numero minimo 1 numero maximo 3")
