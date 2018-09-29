c1702aada39e626c5081f38162fb1ebcbd14dd77
nextorr <nextorr@gmail.com>
2018-09-28 21:25:56 -0500

    paso2 - se crea prueba para 1 elemento con minimo y maximo (ROJO)
    paso2 - se crea prueba para 1 elemento con minimo y maximo (ROJO)

--- ciclo3.py
+++ ciclo3.py
@@ -5,3 +5,6 @@ from Arreglo import Arreglo
 class ciclo2(TestCase):
     def test_cero_elementos(self):
         self.assertEqual(Arreglo().numeroElementos(""),[0,0,0],"calculo de elementos vacio")
+
+    def test_unNumeroUnMinimoUnMaximo(self):
+        self.assertEqual(Arreglo().numeroElementos("1"), [1, 1, 1], "calculo de elementos 1 numero minimo 1 numero maximo 1")
