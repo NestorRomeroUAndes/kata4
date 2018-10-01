1da14a0fa95f0dd2b11d359103cd542bc2601251
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2018-09-30 20:35:58 -0500

    Paso 5:  Refactoring del código para probar el ciclo3 (AZUL)

---
+++ ArregloCiclo3.py
@@ -0,0 +1,10 @@
+class ArregloCiclo3:
+    def numeroElementos(self, cadena):
+        cadena2 = cadena.split(",")
+        tamano = len(cadena2)
+        if len(cadena) == 0:
+            return [0, 0, 0]
+        else:
+            minimo = int(min(cadena2))
+            maximo = int(max(cadena2))
+            return [tamano, minimo, maximo]
--- ciclo3.py
+++ ciclo3.py
@@ -1,16 +1,16 @@
 from unittest import TestCase
 
-from Arreglo import Arreglo
+from ArregloCiclo3 import ArregloCiclo3
 
-class ciclo2(TestCase):
+class ciclo3(TestCase):
     def test_cero_elementos(self):
-        self.assertEqual(Arreglo().numeroElementos(""),[0,0,0],"calculo de elementos vacio")
+        self.assertEqual(ArregloCiclo3().numeroElementos(""),[0,0,0],"calculo de elementos vacio")
 
     def test_unNumeroUnMinimoUnMaximo(self):
-        self.assertEqual(Arreglo().numeroElementos("1"), [1, 1, 1], "calculo de elementos 1 numero minimo 1 numero maximo 1")
+        self.assertEqual(ArregloCiclo3().numeroElementos("1"), [1, 1, 1], "calculo de elementos 1 numero minimo 1 numero maximo 1")
 
     def test_DosNumeroUnMinimoUnMaximo(self):
-        self.assertEqual(Arreglo().numeroElementos("1,3"), [2, 1, 3], "calculo de dos elementos numero minimo 1 numero maximo 3")
+        self.assertEqual(ArregloCiclo3().numeroElementos("1,3"), [2, 1, 3], "calculo de dos elementos numero minimo 1 numero maximo 3")
 
     def test_cualquierNumeroUnMinimoUnMaximo(self):
-        self.assertEqual(Arreglo().numeroElementos("1,3,5,8"), [4, 1, 8], "calculo de dos elementos numero minimo 1 numero maximo 3")
+        self.assertEqual(ArregloCiclo3().numeroElementos("1,3,5,8"), [4, 1, 8], "calculo de dos elementos numero minimo 1 numero maximo 3")
